de336e5e61d7a98c9066c2323df75a199f448aff
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2024-05-31 10:34:35 -0500

    cambis

--- php20/subasta/notificacion.php
+++ php20/subasta/notificacion.php
@@ -64,7 +64,7 @@ if(!isset($_SESSION["email"])) {
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.css" rel="stylesheet">
   </head>
-  <body class="d-flex align-items-center py-4 bg-body-tertiary">
+  <body class="align-items-center bg-body-tertiary">
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
       <symbol id="check2" viewBox="0 0 16 16">
         <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
