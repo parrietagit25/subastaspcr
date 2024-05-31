7be4fb06c789172b749804a63396c34e6c1d3938
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2024-05-31 12:10:41 -0500

    cambis

--- php20/subasta/notificacion.php
+++ php20/subasta/notificacion.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
       foreach ($rows as $key => $value) {
 
-        echo $row['email'].'<br>';
+        echo $value['email'].'<br>';
 
         /*
         try {
