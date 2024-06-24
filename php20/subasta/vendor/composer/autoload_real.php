cb88ae4a07eabae7b84b12fd739b95365c868248
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2024-06-24 13:48:05 -0500

    ultimos cambios

--- php20/subasta/vendor/composer/autoload_real.php
+++ php20/subasta/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit39001c86ae04650c192382a36b025773
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit39001c86ae04650c192382a36b025773::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
