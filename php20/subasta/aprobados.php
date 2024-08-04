42403fd1f61ea31ec2f2b03bb715e5d7448cf292
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2024-08-04 15:12:58 -0500

    ultimos cambios

--- php20/subasta/aprobados.php
+++ php20/subasta/aprobados.php
@@ -263,35 +263,7 @@ if (isset($_POST['edit_user'])) {
       </ul>
     </div>
 
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Main navigation">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Subastas</a>
-            <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="main.php">Inicio</a>
-                </li>
-                
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Registros</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="main.php">Solicitud</a></li>
-                    <li><a class="dropdown-item" href="#">Aprobados</a></li>
-                    <li><a class="dropdown-item" href="notificacion.php">Notificaciones</a></li>
-                    <li><a class="dropdown-item" href="#">API</a></li>
-                </ul>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Salir</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+      <?php include('manu.php'); ?>
 
     <!-- Modal adjuntos -->
     <div class="modal fade" id="adjuntos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
