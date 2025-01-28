4bd39cb20bc1d206ad2a2b031a3af6731b02ae4f
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-01-28 18:26:23 -0500

    cambios

--- php20/subasta/adjuntos.php
+++ php20/subasta/adjuntos.php
@@ -206,7 +206,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_subir_adjunto'])) 
             <h1 class="modal-title fs-5" id="exampleModalLabel">Subir Adjuntos</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <form action="" method="post">
+          <form action="" method="post" enctype="multipart/form-data">
             <div class="modal-body" id="adjuntos_conte_subir">
                 <div class="mb-3">
                     <label for="adjunto" class="form-label">Adjunto</label>
