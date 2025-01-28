1e13d27236d93f65ca3e600eb79fbb211423af8f
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-01-28 12:54:36 -0500

    cambios

--- php20/subasta/usuarios.php
+++ php20/subasta/usuarios.php
@@ -324,7 +324,7 @@ if (isset($_POST['editar_usuario'])) {
                     <td><?php echo $row['stat']; ?></td>\
                     <td>
 
-                      <?php if($_SESSION["id"] == 42){ ?>
+                      <?php if($_SESSION["tipo_user"] == 'admin'){ ?>
                         <a type="button" class="btn btn-success btn-icon" data-bs-toggle="modal" data-bs-target="#editar_users" onclick="aprobar(<?php echo $row['id']; ?>)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                 <path d="M12.146.854a.5.5 0 0 1 .708 0l2.292 2.292a.5.5 0 0 1 0 .708l-9.396 9.396a.5.5 0 0 1-.168.11l-4.39 1.464a.5.5 0 0 1-.641-.641l1.464-4.39a.5.5 0 0 1 .11-.168l9.396-9.396zm-.646.646L3.5 9.5V12h2.5l8-8L11.5 1.5zm1.207-.707L13.5 2.793 14.207 2 13.5 1.5z"/>
@@ -333,7 +333,7 @@ if (isset($_POST['editar_usuario'])) {
 
                       <?php } ?>
 
-                      <?php if($_SESSION["id"] == 42){ ?>
+                      <?php if($_SESSION["tipo_user"] == 'admin'){ ?>
                       <a type="button" class="btn btn-danger btn-icon" data-bs-toggle="modal" data-bs-target="#eliminar" onclick="eliminar(<?php echo $row['id']; ?>)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
